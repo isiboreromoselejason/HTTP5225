44b2d4f135e4acf7e862b9a74a954b474b330431
Isibor Eromosele <isiboreromoselejason@gmail.com>
2024-05-15 16:19:04 -0400

    Update index.php

---
+++ Web Development Project - HTTP-5225-0NA/week 2/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>week 2</title>
+</head>
+<body>
+
+<?php
+
+?>
+<h1> Welcome to HTTP 5225</h1>
+<p>we will learn Php in this class</p>
+<p>Things we will learn in the class</p>
+<ul>
+    <li>PHP</li>
+    <li>mySQL</li>
+    <li>Laravel</li>
+</ul>
+<img src="" alt="">
+    
+</body>
+</html>
